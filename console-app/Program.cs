7d011cb04b5ce23101c4f108f4f5e8aa729f7335
Jeremy Barrass <jeremy.barrass@gmail.com>
2021-03-07 13:09:47 +0000

    start console app.

---
+++ console-app/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+
+namespace console_app
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            var result = client.GetAsync("https://localhost:5001/Products").Result;
+
+            Console.Write(result);
+        }
+    }
+}
